579017ab3cef2bbe3e9f8132dd1d2da5defb90b0
Wilson Agyapong <wilsonagyapong16@gmail.com>
2024-07-23 14:36:22 -0700

    created new api key

--- client/src/pages/Results.jsx
+++ client/src/pages/Results.jsx
@@ -2,24 +2,32 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Navbar from '../components/Navbar.jsx';
 
-let API_key = 'dfc7d82446703b884e41a688a9a1bb43';
 let base_url = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_API_KEY; // Use REACT_APP_ prefix
 
 export function Results() {
   const [results, setResults] = useState([]);
+  const [query, setQuery] = useState("");
   const location = useLocation();
+
+  console.log(API_KEY);
 
   useEffect(() => {
     const params = new URLSearchParams(location.search);
-    const query = params.get('query');
+    const queryvar = params.get('query');
 
-    if (query) {
-      const searchUrl = `${base_url}/search/movie?api_key=${API_key}&query=${query}`;
+    if (queryvar) {
+      setQuery(queryvar);
+      const searchUrl = `${base_url}/search/movie?api_key=${API_KEY}&query=${queryvar}`;
+      console.log(searchUrl);
       fetch(searchUrl)
         .then(res => res.json())
         .then(data => {
           console.log(data.results);
           setResults(data.results);
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
         });
     }
   }, [location.search]);
@@ -27,13 +35,17 @@ export function Results() {
   return (
     <div>
       <Navbar />
-      <h1>Search Results</h1>
+      <h1 className="text-4xl my-4">{`Search Results for ${query}`}</h1>
       <ul>
         {results.map(result => (
           <li key={result.id}>
-            <img src={`https://image.tmdb.org/t/p/original/${result.poster_path}`} alt={result.title} width="200px"/>
-            {result.title}  
-            </li>
+            <img
+              src={`https://image.tmdb.org/t/p/original/${result.poster_path}`}
+              alt={result.title}
+              width="200px"
+            />
+            {result.title}
+          </li>
         ))}
       </ul>
     </div>
